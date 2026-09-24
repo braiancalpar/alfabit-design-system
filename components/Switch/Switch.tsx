29c6c9cc3da7a3c62c17492e987335449210b06b
braiancalpar <braian.santos@calpar.com.br>
2026-09-24 11:19:59 -0300

    adiciona componente Switch com suas histórias e documentação no Storybook, incluindo estilos e integração com @headlessui/react

---
+++ components/Switch/Switch.tsx
@@ -0,0 +1,48 @@
+import {
+  Switch as HeadlessSwitch,
+  type SwitchProps as HeadlessSwitchProps,
+} from "@headlessui/react";
+import { useState } from "react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
+
+export type SwitchProps = {
+  defaultEnable?: boolean;
+  variant?: "common" | "contract";
+  disabled?: boolean;
+  onChange?: (enabled: boolean) => void;
+} & HeadlessSwitchProps<any>;
+
+const Switch = ({
+  defaultEnable: enabledByDefault,
+  variant = "common",
+  disabled,
+  onChange,
+  ...rest
+}: SwitchProps) => {
+  const [enabled, setEnabled] = useState(enabledByDefault);
+
+  const toggle = () => {
+    const newState = !enabled;
+    setEnabled(newState);
+    onChange?.(newState);
+  };
+
+  return (
+    <HeadlessSwitch
+      checked={enabled}
+      onChange={toggle}
+      disabled={disabled}
+      {...rest}
+    >
+      {variant === "common" && <span />}
+      {variant === "contract" && (
+        <span>
+          {enabled && <CheckIcon aria-disabled={disabled} />}
+          {enabled || <XMarkIcon aria-disabled={disabled} />}
+        </span>
+      )}
+    </HeadlessSwitch>
+  );
+};
+
+export default Switch;
